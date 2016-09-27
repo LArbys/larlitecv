167068e4960aeb60ac8730eba44d528256de7a63
Taritree Wongjirad <twongjirad@MacBook-Air.local>
2016-09-27 14:46:48 -0400

    save

--- core/Base/DataCoordinator.h
+++ core/Base/DataCoordinator.h
@@ -49,6 +49,9 @@ namespace larlitecv {
     void goto_entry( int entry, std::string ftype );
     void goto_event( int run, int subrun, int event );
 
+    // saving output
+    void save_entry();
+
     // load after specifying files
     void initialize();
     void finalize();
