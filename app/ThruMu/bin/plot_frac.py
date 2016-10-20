191ac952933ef3356417d2177857b7a6e323b53b
Taritree Wongjirad <twongjirad@MacBook-Air.local>
2016-10-20 10:59:21 -0400

    added analysis tools

---
+++ app/ThruMu/bin/plot_frac.py
@@ -0,0 +1,45 @@
+import os,sys
+import ROOT as rt
+
+rt.gStyle.SetOptStat(0)
+
+fgood = open("spoon_roi_scan.csv",'r')
+lgood = fgood.readlines()
+
+goodroi = []
+for l in lgood[1:]:
+    u = int(l.split(",")[1].strip())
+    v = int(l.split(",")[2].strip())
+    y = int(l.split(",")[3].strip())
+    goodroi.append( [u,v,y] )
+
+fin = rt.TFile("output_analysis.root","open")
+bmt = fin.Get("bmt")
+
+hnu  = rt.TH1D("hnu","",10,0,1.1)
+htot = rt.TH1D("htot","",10,0,1.1)
+
+nentries = bmt.GetEntries()
+for i in range(0,nentries):
+    bmt.GetEntry(i)
+    if bmt.dwall<5:
+        continue
+    for p in range(0,3):
+        totfrac = bmt.total_frac_remain[p]
+        nufrac   = bmt.nubb_frac_remain[p]
+        if True or goodroi[i][p]==1:
+            htot.Fill( totfrac )
+            hnu.Fill( nufrac )
+            if nufrac<0.9:
+                print i,p,nufrac
+
+c = rt.TCanvas("c","c",800,400)
+c.Draw()
+hnu.SetLineColor(rt.kRed)
+hnu.Draw()
+htot.Draw("same")
+
+c.Update()
+
+print "[enter] to exit"
+raw_input()
