3cdc3b42d4c4d2455aa8bc8de1fafec2a7fea647
Taritree Wongjirad <twongjirad@macbook-air.dhcp.fnal.gov>
2016-11-14 12:52:32 -0600

    analysis runs on file list now

--- app/ThruMu/bin/plot_frac.py
+++ app/ThruMu/bin/plot_frac.py
@@ -5,6 +5,8 @@ rt.gStyle.SetOptStat(0)
 
 fgood = open("spoon_roi_scan.csv",'r')
 lgood = fgood.readlines()
+use_handscan = False
+use_dwallcut = False
 
 goodroi = []
 for l in lgood[1:]:
@@ -16,22 +18,26 @@ for l in lgood[1:]:
 fin = rt.TFile("output_analysis.root","open")
 bmt = fin.Get("bmt")
 
-hnu  = rt.TH1D("hnu","",10,0,1.1)
-htot = rt.TH1D("htot","",10,0,1.1)
+hnu  = rt.TH1D("hnu","",50,0,1.001)
+htot = rt.TH1D("htot","",50,0,1.001)
 
 nentries = bmt.GetEntries()
+nbelow90 = 0
 for i in range(0,nentries):
     bmt.GetEntry(i)
-    if bmt.dwall<5:
+    if use_dwallcut and bmt.dwall<5:
         continue
     for p in range(0,3):
         totfrac = bmt.total_frac_remain[p]
         nufrac   = bmt.nubb_frac_remain[p]
-        if True or goodroi[i][p]==1:
-            htot.Fill( totfrac )
-            hnu.Fill( nufrac )
-            if nufrac<0.9:
-                print i,p,nufrac
+        if not use_handscan or goodroi[i][p]==1:
+            if False or ( bmt.enu<500 and bmt.enu>100 and bmt.dwall>5):
+                htot.Fill( totfrac )
+                hnu.Fill( nufrac )
+                if nufrac<0.9:
+                    print i,(bmt.run,bmt.subrun,bmt.event),p,nufrac
+                    nbelow90+=1
+print "below 90: ",float(nbelow90)/float(hnu.Integral())
 
 c = rt.TCanvas("c","c",800,400)
 c.Draw()
