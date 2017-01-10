eca60e8ba72a0ef95a5a433724185f0e8be47594
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-01-10 17:33:18 -0500

    added more truth information extraction

--- app/AnalyzeTagger/run_pixel_analysis.cxx
+++ app/AnalyzeTagger/run_pixel_analysis.cxx
@@ -7,32 +7,54 @@
 
 // larlite
 #include "DataFormat/mctruth.h"
+#include "DataFormat/mcpart.h"
+#include "DataFormat/mctrajectory.h"
 #include "LArUtil/LArProperties.h"
 #include "LArUtil/Geometry.h"
 
 #include "TFile.h"
 #include "TTree.h"
 
-float dwall( const std::vector<float>& pos ) {
+float dwall( const std::vector<float>& pos, int& boundary_type ) {
 
   float dx1 = fabs(pos[0]);
-  float dx2 = fabs(260.0-pos[0]);
-  float dy1 = fabs(118.0-pos[1]);
-  float dy2 = fabs(-118.0-pos[1]);
+  float dx2 = fabs(255-pos[0]);
+  float dy1 = fabs(116.0-pos[1]);
+  float dy2 = fabs(-116.0-pos[1]);
   float dz1 = fabs(pos[2]);
   float dz2 = fabs(1036.0-pos[2]);
 
   float dwall = 1.0e9;
-  if ( dx1<dwall ) dwall = dx1;
-  if ( dx2<dwall ) dwall = dx2;
-  if ( dy1<dwall ) dwall = dy1;
-  if ( dy2<dwall ) dwall = dy2;
-  if ( dz1<dwall ) dwall = dz1;
-  if ( dz2<dwall ) dwall = dz2;
+
+  if ( dy1<dwall ) {
+  	dwall = dy1;
+  	boundary_type = 0;
+  }
+  if ( dy2<dwall ) {
+  	dwall = dy2;
+  	boundary_type = 1;
+  }
+  if ( dz1<dwall ) {
+  	dwall = dz1;
+  	boundary_type = 2;
+  }
+  if ( dz2<dwall ) {
+  	dwall = dz2;
+  	boundary_type = 3;
+  }
+  if ( dx1<dwall ) {
+  	dwall = dx1;
+  	boundary_type = 4;
+  }
+  if ( dx2<dwall ) {
+  	dwall = dx2;
+  	boundary_type = 5;
+  }
 
   return dwall;
 
 }
+
 
 int main( int nargs, char** argv ) {
 
@@ -70,27 +92,35 @@ int main( int nargs, char** argv ) {
   larcv::PSet cfg = larcv::CreatePSetFromFile( "config.cfg" );
   larcv::PSet pixana_cfg = cfg.get<larcv::PSet>("PixelAnalysis");
   float fthreshold   = pixana_cfg.get<float>("PixelThreshold");
-  int fvertex_radius =  pixana_cfg.get<int>("PixelRadius");
+  int fvertex_radius = pixana_cfg.get<int>("PixelRadius");
+  int verbosity      = pixana_cfg.get<int>("Verbosity",0);
 
   // setup output
 
   TFile* rfile = new TFile("output_pixel_analysis.root", "recreate");
   TTree* tree = new TTree("pixana", "Pixel-level analysis");
-  int ncosmic_pixels; // number of non-neutrino pixels
-  int nnu_pixels;     // number of neutrino pixels
-  int nvertex_pixels; // number of neutrino pixels within some pixel radius of vertex
-  int ncosmic_tagged; // number of non-neutrino pixels tagged
-  int nnu_tagged;     // number of neutrino pixels tagged
-  int nvertex_tagged; // number of neutrino pixels within some pixel radius of vertex tagged
-  int mode;           // interaction mode
-  int current;        // interaction cufrrent
-  float EnuGeV;       // neutrino energy in GeV
-  float fdwall;        // dwall
-  float frac_cosmic;  // fraction of cosmic tagged
-  float frac_nu;      // fraction of neutrino pixels tagged
-  float frac_vertex;  // fraction of vertex pixels tagged
-  float fpos[3];
   int run, subrun, event;
+  int lepton_boundary; // index of boundary that lepton end point is nearest
+  int vertex_boundary; // index of boundary that vertex is nearest
+  int ncosmic_pixels;  // number of non-neutrino pixels
+  int nnu_pixels;      // number of neutrino pixels
+  int nvertex_pixels;  // number of neutrino pixels within some pixel radius of vertex
+  int ncosmic_tagged;  // number of non-neutrino pixels tagged
+  int nnu_tagged;      // number of neutrino pixels tagged
+  int nvertex_tagged;  // number of neutrino pixels within some pixel radius of vertex tagged
+  int mode;            // interaction mode
+  int current;         // interaction cufrrent
+  int num_protons_over60mev; // as named
+  float EnuGeV;        // neutrino energy in GeV
+  float fdwall;        // dwall
+  float dwall_lepton;  // dwll for end of lepton
+  float frac_cosmic;   // fraction of cosmic tagged
+  float frac_nu;       // fraction of neutrino pixels tagged
+  float frac_vertex;   // fraction of vertex pixels tagged
+  float primary_proton_ke; // ke of the proton driving from the hit nucleon
+  float lepton_cosz;
+  float lepton_phiz;
+  float fpos[3];       // vertex
   tree->Branch("run",&run,"run/I");
   tree->Branch("subrun",&subrun,"subrun/I");
   tree->Branch("event",&event,"event/I");
@@ -102,12 +132,19 @@ int main( int nargs, char** argv ) {
   tree->Branch("nvertex_tagged",&nvertex_tagged,"nvertex_tagged/I");
   tree->Branch("mode",&mode,"mode/I");
   tree->Branch("current",&current,"current/I");
+  tree->Branch("lepton_boundary",&lepton_boundary,"lepton_boundary/I");
+  tree->Branch("vertex_boundary",&vertex_boundary,"vertex_boundary/I");
+  tree->Branch("num_protons_over60mev", &num_protons_over60mev, "num_protons_over60mev/I");
   tree->Branch("EnuGeV",&EnuGeV,"EnuGeV/F");
   tree->Branch("dwall",&fdwall,"dwall/F");
+  tree->Branch("dwall_lepton",&dwall_lepton,"dwall_lepton/F");
   tree->Branch("frac_cosmic",&frac_cosmic,"frac_cosmic/F");
   tree->Branch("frac_nu",&frac_nu,"frac_nu/F");
   tree->Branch("frac_vertex",&frac_vertex,"frac_vertex/F");
+  tree->Branch("primary_proton_ke", &primary_proton_ke, "primary_proton_ke/F");
   tree->Branch("pos",fpos,"pos[3]/F");
+  tree->Branch("lepton_cosz", &lepton_cosz, "lepton_cosz/F");
+  tree->Branch("lepton_phiz", &lepton_phiz, "lepton_phiz/F");
 
   int nentries = dataco_stopmu.get_nentries("larcv");
 
@@ -121,9 +158,9 @@ int main( int nargs, char** argv ) {
 
     dataco_stopmu.get_id(run,subrun,event);
 
-    if ( ientry%10==0 ) {
+    if ( ientry%10==0 || verbosity>0 ) {
       std::cout << "entry " << ientry << std::endl;
-      std::cout << " (r,s,e)=(" << run << ", " << " , " << subrun << ", " << event << ")" << std::endl;
+      std::cout << " (r,s,e)=(" << run << ", " << subrun << ", " << event << ")" << std::endl;
     }
 
     dataco_thrumu.goto_event(run,subrun,event,"larcv");
@@ -144,6 +181,8 @@ int main( int nargs, char** argv ) {
     frac_vertex = 0.;
     frac_nu = 0.;
     fpos[0] = fpos[1] = fpos[2] = 0.;
+    num_protons_over60mev = 0;
+    primary_proton_ke = 0.;
 
     // ok now to do damage
 
@@ -178,7 +217,9 @@ int main( int nargs, char** argv ) {
     dpos[0] = nu_pos.X();
     dpos[1] = nu_pos.Y();
     dpos[2] = nu_pos.Z();
-    fdwall = dwall(fpos_v);
+    fdwall = dwall(fpos_v, vertex_boundary);
+    if ( verbosity>0 )
+	  	std::cout << " Enu=" << EnuGeV << std::endl;
 
     // get the vertex in the pixel coordinates
     std::vector<int> wid(3,-1);
@@ -186,7 +227,7 @@ int main( int nargs, char** argv ) {
     for (size_t p=0; p<3; p++) {
       wid[p] = ::larutil::Geometry::GetME()->WireCoordinate( dpos, p );
       if ( wid[p]>=0 && wid[p]<3456 )
-	vertex_col[p] = imgs_v.at(p).meta().col(wid[p]);
+				vertex_col[p] = imgs_v.at(p).meta().col(wid[p]);
       fpos[p] = fpos_v[p];
     }
     float cm_per_tick = ::larutil::LArProperties::GetME()->DriftVelocity()*0.5;
@@ -194,6 +235,75 @@ int main( int nargs, char** argv ) {
     int vertex_row = -1;
     if ( ftick >= imgs_v.at(0).meta().min_y() && ftick<=imgs_v.at(0).meta().max_y() )
       vertex_row = imgs_v.at(0).meta().row( ftick );
+
+    // get the initial direction of the leptop
+    const std::vector<larlite::mcpart>& particles = ev_mctruth->at(0).GetParticles();
+    bool found_lepton = false;
+    int hit_nucleon_id = 2;
+    std::set<int> protonids;
+    for ( auto  const& particle : particles ) {
+    	float KE = particle.Trajectory().front().E() - particle.Mass();
+	    if ( !found_lepton && (particle.PdgCode()==13 || particle.PdgCode()==-13) ) {
+	    	// found the lepton
+	    	const larlite::mctrajectory& traj = particle.Trajectory();
+	    	const larlite::mcstep& last_step = traj.back();
+	    	const larlite::mcstep& first_step = traj.front();
+	    	std::cout << "  lepton E=" << particle.Trajectory().front().E() << " KE=" << KE
+	    		<< "  last step position: "
+	    		<< "(" << last_step.X()  << "," << last_step.Y() << "," << last_step.Z() << ")"
+	    		<< std::endl;
+	    	std::vector<float> lepton_end(3);
+	    	lepton_end[0] = last_step.X();
+	    	lepton_end[1] = last_step.Y();
+	    	lepton_end[2] = last_step.Z();
+	    	float norm = 0.;
+	    	std::vector<float> lepton_dir(3);
+	    	lepton_dir[0] = first_step.Momentum().Vect().X();
+	    	lepton_dir[1] = first_step.Momentum().Vect().Y();
+	    	lepton_dir[2] = first_step.Momentum().Vect().Z();
+	    	for (int v=0; v<3; v++) norm += lepton_dir[v]*lepton_dir[v];
+	    	norm = sqrt(norm);
+	    	for (int v=0; v<3; v++) lepton_dir[v] /= norm;
+	    	lepton_cosz = lepton_dir[2];
+	    	lepton_phiz = atan2( lepton_dir[1], lepton_dir[0] );
+
+	    	dwall_lepton = dwall( lepton_end, lepton_boundary );
+	    	found_lepton = true;
+	    }
+	    else if ( particle.PdgCode()==2212 ) {
+	    	std::cout << "  a proton. p=" << particle.Momentum(0).Vect().Mag() 
+	    		<< " E=" << particle.Trajectory().front().E() << " KE=" << KE
+	    		<< " status=" << particle.StatusCode() 
+	    		<< " trackid=" << particle.TrackId() << " mother=" << particle.Mother()
+	    		<< std::endl;
+	    	if ( particle.StatusCode()!=11 && KE>0.060 && protonids.find(particle.Mother())==protonids.end() ) 
+	    		num_protons_over60mev++; // status 11 means from genie? threshold cut. check that it isn't from the original proton
+	    	protonids.insert( particle.TrackId() );
+	    }
+	    else if ( particle.PdgCode()==14 || particle.PdgCode()==-14 ) {
+	    	std::cout << "  the neutrino (pdg=" << particle.PdgCode() << ") Enu=" << particle.Trajectory().front().E() << std::endl;
+	    } 
+	    else {
+	    	std::cout << "  pdg=" << particle.PdgCode() 
+	    		<< " E=" << particle.Trajectory().front().E() << " KE=" << KE
+	    		<< " status=" << particle.StatusCode() 
+	    		<< " end process=" << particle.EndProcess()
+	    		<< " trackid=" << particle.TrackId() << " mother=" << particle.Mother() << std::endl;
+	    }
+
+	    // stuff we are saving
+	    if ( (particle.PdgCode()==2212 || particle.PdgCode()==2112) && particle.StatusCode()==11 ) {
+	    	hit_nucleon_id = particle.TrackId();
+	    }
+	    if ( particle.PdgCode()==2212 && particle.Mother()==hit_nucleon_id ) {
+	    	primary_proton_ke = KE;
+	    	protonids.insert(particle.TrackId());
+	    }
+
+    }//end of particle track loop
+
+    std::cout << "num_protons_over60mev=" << num_protons_over60mev << std::endl;
+    std::cout << "primary_proton_ke=" << primary_proton_ke << std::endl;
 
     // count the pixels. we loop through the rows and cols
     for (size_t p=0; p<3; p++) {
