3c834b2dfe5d30c57bfd64fd8a092ca14258da19
Christopher Barnes <christopherbarnes@Christopher-Barness-MacBook-Pro-3.local>
2017-02-26 21:47:21 -0500

    AStar3DPostProcessor.cxx

--- app/ThruMu/Linear3DPostProcessor.h
+++ app/ThruMu/Linear3DPostProcessor.h
@@ -19,6 +19,10 @@
 
 #include <vector>
 
+// larlite/UserDev/BasicTool/GeoAlgo
+#include "GeoAlgo.h"
+#include "GeoLineSegment.h"
+
 // larlitecv/app/Thrumu
 #include "BMTrackCluster3D.h"
 
@@ -30,8 +34,17 @@ namespace larlitecv {
     Linear3DPostProcessor() {};
     virtual ~Linear3DPostProcessor() {};
 
-    std::vector< BMTrackCluster3D > process( std::vector< BMTrackCluster3D >& tracks_v );
-    
+    std::vector < BMTrackCluster3D > process( std::vector< BMTrackCluster3D >& tracks_v );
+
+    bool pointProximity(geoalgo::Line long_line, geoalgo::LineSegment* short_linesegment, int resolution, double maximum_dist);
+
+    int longestDimFinder(const std::vector <double> point_one_coords, const std::vector <double> point_two_coords);
+
+    std::vector < std::vector<double> > getTrackPoints(const std::vector<double> point_one_coords, const std::vector<double> point_two_coords, int resolution, std::vector <int> shorter_dim_idx_vector, int longest_dim_idx);
+      
+    std::vector <std::vector <double> > longerTrackExtensionCoordinates(std::vector<double> longer_line_first_point_coords, std::vector<double> longer_line_second_point_coords, std::vector<double> shorter_linesegment_first_point_coords, std::vector<double> shorter_linesegment_second_point_coords);
+
+    void  concatenatingTwoTrackParts(std::vector< BMTrackCluster3D >& tracks_v, int itrack_a, int itrack_b, BMTrackCluster3D& track_a, BMTrackCluster3D& track_b, std::vector < std::vector <double> > endpoint_coordinates_of_new_track);
 
   };
 
