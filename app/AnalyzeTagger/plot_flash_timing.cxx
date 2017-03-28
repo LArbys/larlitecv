404e6646d397b947cb974de18fc5c0d61246102c
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-03-27 20:00:19 -0400

    flashes matched. poorly

--- app/AnalyzeTagger/plot_flash_timing.cxx
+++ app/AnalyzeTagger/plot_flash_timing.cxx
@@ -1,4 +1,5 @@
-#include "Base/DataCoordinator.h"
+#include <iostream>
+#include <vector>
 
 // larcv
 #include "DataFormat/EventImage2D.h"
@@ -12,6 +13,13 @@
 #include "DataFormat/track.h"
 #include "LArUtil/Geometry.h"
 
+// larlitecv
+#include "Base/DataCoordinator.h"
+#include "ContainedROI/TaggerFlashMatchAlgo.h"
+#include "ContainedROI/FlashMatchMetricMethods.h"
+#include "extractTruthMethods.h"
+
+// ROOT
 #include "TFile.h"
 #include "TTree.h"
 
@@ -52,10 +60,15 @@ int main( int nargs, char** argv ) {
   beam_tick_range[0] = 100;
   beam_tick_range[1] = 400.0;
   const float us_per_tick = 0.015625;
+  const float bbox_buffer = 10.0;
+
+  // Flash Match Metrics we want to test
 
   TFile* rfile = new TFile("output_flash_timing.root", "recreate");
   TTree* tree = new TTree("flashtimes", "Flash Times");
   int run, subrun, event;
+
+  // details about data flash
   int flash_time_tick;
   float flash_time_us;  // dwll for end of lepton
   float flash_dtrigger_us;
@@ -65,6 +78,23 @@ int main( int nargs, char** argv ) {
   tree->Branch("flash_time_tick", &flash_time_tick, "flash_time_tick/I" );
   tree->Branch("flash_time_us", &flash_time_us, "flash_time_us/F" );
   tree->Branch("flash_dtrigger_us", &flash_dtrigger_us, "flash_dtrigger_us/F" );
+
+
+  // flash metrics output
+  std::vector<float> smallest_chi2_falseflash_v; // chi2 to flash hypothesis not corresponding to true source of trigger
+  std::vector<float> smallest_chi2_trueflash_v;  // chi2 to flash hypothesis corresponding to true source of trigger
+  std::vector<float> totpe_data_v;               // total pe for in-time flashes in the data
+  std::vector<float> totpe_hypo_trueflash_v;     // total pe for correct flash hypotheses
+  std::vector<float> totpe_hypo_falseflash_v;    // total pe for flase flash hypotheses
+  tree->Branch("smallest_chi2_falseflashes", &smallest_chi2_falseflash_v );
+  tree->Branch("smallest_chi2_trueflashes",  &smallest_chi2_trueflash_v );
+  tree->Branch("totpe_data",                 &totpe_data_v );
+  tree->Branch("totpe_hypo_trueflashes",     &totpe_hypo_trueflash_v );
+  tree->Branch("totpe_hypo_falseflashes",    &totpe_hypo_falseflash_v );
+
+  // Truth Quantities about interaction and lepton
+  larlitecv::TruthData_t truthdata;
+  truthdata.bindToTree( tree );
 
   int nentries = dataco[kCROIfile].get_nentries("larcv");
 
@@ -82,6 +112,14 @@ int main( int nargs, char** argv ) {
     std::cout << "entry " << ientry << " (r,s,e)=(" << run << ", " << subrun << ", " << event << ")" << std::endl;
     std::cout << "---------------------------------------------" << std::endl;
 
+    // clear variables
+    truthdata.clear();
+    smallest_chi2_trueflash_v.clear();
+    smallest_chi2_falseflash_v.clear();
+    totpe_data_v.clear();
+    totpe_hypo_trueflash_v.clear();
+    totpe_hypo_falseflash_v.clear();
+
     // we get wnat to get:
     //  (1) truth info
     //  (2) "data" in time opflashes
@@ -89,17 +127,30 @@ int main( int nargs, char** argv ) {
     //  (4) calculate metrics for flash matching
     //  (5) split flash hypotheses coming from "correct" and "incorrect" hypotheses
 
+    // get truth data
+    // --------------
+    larlite::event_mctruth* ev_mctruth   = (larlite::event_mctruth*) dataco[kSource].get_larlite_data(larlite::data::kMCTruth,"generator");
+    larlite::event_mctrack* ev_mctrack   = (larlite::event_mctrack*) dataco[kSource].get_larlite_data(larlite::data::kMCTrack,"mcreco");
+    larlite::event_mcshower* ev_mcshower = (larlite::event_mcshower*)dataco[kSource].get_larlite_data(larlite::data::kMCShower,"mcreco");
+    larlite::trigger* ev_trigger         = (larlite::trigger*)       dataco[kSource].get_larlite_data(larlite::data::kTrigger,"triggersim");
+    larlitecv::extractTruth( *ev_mctruth, *ev_mctrack, truthdata );
+
     // get flash data
     // --------------
     larlite::event_opflash* ev_data_opflash = (larlite::event_opflash*)dataco[kSource].get_larlite_data( larlite::data::kOpFlash, "simpleFlashBeam" );
     larlite::event_opflash* ev_data_ophypo  = (larlite::event_opflash*)dataco[kCROIfile].get_larlite_data( larlite::data::kOpFlash, "ophypo" );
 
-    // Get in time beam flashes
+    // select in time beam flashes
     std::vector<larlite::opflash> intime_data;
     for ( auto const& flash : *ev_data_opflash ) {
       int tick = flash.Time()/us_per_tick;
-      if ( tick>=beam_tick_range[0] && tick<=beam_tick_range[1] )
+      if ( tick>=beam_tick_range[0] && tick<=beam_tick_range[1] ) {
         intime_data.push_back( flash );
+        float totpe_data = 0.;
+        for (int ich=0; ich<32; ich++)
+          totpe_data += flash.PE(ich);
+        totpe_data_v.push_back( totpe_data );
+      }
     }
     std::cout << "Number of in-time (data) flashes: " << intime_data.size() << std::endl;
     std::cout << "Number of in-time flash hypotheses: " << ev_data_ophypo->size() << std::endl;
@@ -112,20 +163,54 @@ int main( int nargs, char** argv ) {
     for ( int istage=0; istage<=kUntagged; istage++ ) {
       ev_tagger_tracks[istage] = (larlite::event_track*)dataco[kCROIfile].get_larlite_data( larlite::data::kTrack, stages_track_producers[istage] );
       for ( auto const& track : *(ev_tagger_tracks[istage]) ) {
-        if ( track.NumberTrajectoryPoints()==0 )
-          continue;
+        //if ( track.NumberTrajectoryPoints()==0 )
+        //  continue;
         track_list.push_back( &track );
       }
     }
     std::cout << "Number of tracks: " << track_list.size() << std::endl;
 
     if ( track_list.size()!=ev_data_ophypo->size() ) {
+      continue; // freak out and skip event
       throw std::runtime_error("Number of tracks and number of flash hypotheses do not match.");
     }
 
+    // loop through tracks and flash hypotheses. Get metrics for each.
+    int num_true_bbox = 0;
+    for ( size_t itrack=0; itrack<track_list.size(); itrack++ ) {
+      const larlite::track& track = *(track_list.at(itrack));
+      const larlite::opflash& ophypo = ev_data_ophypo->at(itrack);
+      float totpe_data = 0;
+      float totpe_hypo = 0.;
+      float smallest_chi2 = larlitecv::CalculateFlashMatchChi2( intime_data, ophypo, totpe_data, totpe_hypo, 20000.0, false );
 
-    if ( ientry>=3)
-      break;
+      // need to determine if this track is the intime track or not
+      // we do this by checking if track gets close to true vertex. slow AF.
+      std::vector< std::vector<float> > bbox = larlitecv::TaggerFlashMatchAlgo::GetAABoundingBox( track );
+      bool inbbox = true;
+      for ( int v=0; v<3; v++ ) {
+        if ( truthdata.pos[v]<bbox[v][0]-bbox_buffer || truthdata.pos[v]>bbox[v][1]+bbox_buffer ) {
+          inbbox = false;
+          break;
+        }
+      }
+
+      if ( inbbox ) {
+        smallest_chi2_trueflash_v.push_back( smallest_chi2 );
+        totpe_hypo_trueflash_v.push_back( totpe_hypo );
+        num_true_bbox++;
+      }
+      else {
+        smallest_chi2_falseflash_v.push_back( smallest_chi2 );
+        totpe_hypo_falseflash_v.push_back( totpe_hypo );
+      }
+    }
+    std::cout << "Number of Vertex Enclosing BBox: " << num_true_bbox << std::endl;
+
+    tree->Fill();
+
+    //if ( ientry>=3)
+    //  break;
   }//end of entry loop
 
   rfile->Write();
