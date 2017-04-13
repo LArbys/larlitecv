60235b7e18792050c27779da892150be545f38c7
Chris Barnes <barnes@nunet.lns.mit.edu>
2017-04-13 10:49:41 -0400

    Changes to Finding a Better OpFlash

--- app/AnalyzeTagger/plot_flash_timing_crb_version_simplified.cxx
+++ app/AnalyzeTagger/plot_flash_timing_crb_version_simplified.cxx
@@ -32,10 +32,13 @@ int main( int nargs, char** argv ) {
 
   std::cout << "STUDY FLASH-MATCH METRICS" << std::endl;
 
-  if ( nargs!=2 ) {
-    std::cout << "usage: ./plot_flash_timing [config file]" << std::endl;
+  if ( nargs!=3 ) {
+    std::cout << "usage: ./plot_flash_timing [config file #1] [config file #2]" << std::endl;
     return 0;
   }
+
+  // Print Statement #0.5
+  std::cout << "Entering the first config file." << std::endl;
 
   // Load Config File
   // The file that I will want to enter here is: 'flashmetrics.cfg'.
@@ -43,10 +46,15 @@ int main( int nargs, char** argv ) {
   larcv::PSet cfg_root = larcv::CreatePSetFromFile( cfg_file );
   larcv::PSet pset = cfg_root.get<larcv::PSet>("FlashMatchAnalysis");
 
+  // Print Statement #0.75
+  std::cout << "Entering the section of code with the data coordinators." << std::endl;
+
   // CRB Code.
   // Do the same for the other config file.
   // The file that I want to enter here is: '../ContainedROI/bin/containedroi.cfg'.
   std::string   FlashMgr_cfg_file      = argv[2];
+  // Print Statement #0.9
+  std::cout << "Entering the statement with the PSetConfiguration." << std::endl;
   larcv::PSet   FlashMgr_cfg_root      = larcv::CreatePSetFromFile( FlashMgr_cfg_file );
   larcv::PSet   FlashMgr_PSet          = FlashMgr_cfg_root.get<larcv::PSet>("ContainedROI").get<larcv::PSet>("TaggerFlashMatchAlgo");
   larlitecv::TaggerFlashMatchAlgoConfig cfg = larlitecv::TaggerFlashMatchAlgoConfig::MakeTaggerFlashMatchAlgoConfigFromPSet( FlashMgr_PSet ); // Give to the instance of class 'TaggerFlashMatchAlgo'.              
@@ -56,6 +64,9 @@ int main( int nargs, char** argv ) {
 
   // Declare a constant at the top for the number of cm in the x-direction per tick in the wire-plane images.
   // const float cm_per_tick = ::larutil::LArProperties::GetME()->DriftVelocity()*0.5; // [cm/usec] * [usec/tick] = [cm/tick]
+
+  // Print Statement #1
+  std::cout << "Making it past the PSet initialization." << std::endl;
 
   // Setup the Data Coordinators
   enum { kSource=0, kCROIfile, kNumSourceTypes } SourceTypes_t;
@@ -67,6 +78,10 @@ int main( int nargs, char** argv ) {
     dataco[isrc].configure( cfg_file, "StorageManager", "IOManager", "FlashMatchAnalysis" );
     dataco[isrc].initialize();
   }
+
+  // Print Statement #2
+  std::cout << "Making it past the input file list initialization." << std::endl;
+  
   for (int isrc=0; isrc<kNumSourceTypes; isrc++ ) {
     std::cout << "data[" << source_param[isrc] << "] entries=" << dataco[isrc].get_nentries("larcv") << std::endl;
   }
@@ -85,6 +100,9 @@ int main( int nargs, char** argv ) {
   // CRB: Include the configuration parameters for the region that I will check for the lowest chisquare value:
   double LowestChiSquareGridHalfSize     = pset.get< double >("LowestChiSquareGridHalfSize");
   double LowestChiSquareGridPointSep     = pset.get< double >("LowestChiSquareGridPointSep");
+
+  // Print Statement #3
+  std::cout << "Making it past the stages track producers initialization." << std::endl;
 
   // Declare variables for the number of columns and the numbers of rows.
   // size_t row_num = 3456;
@@ -113,6 +131,39 @@ int main( int nargs, char** argv ) {
   tree->Branch("flash_time_us", &flash_time_us, "flash_time_us/F" );
   tree->Branch("flash_dtrigger_us", &flash_dtrigger_us, "flash_dtrigger_us/F" );
 
+  // I will declare a new output file for all of my data.
+  TFile* rfile_ana = new TFile("flash_hypo_output_500_events_ana_truth_experiment_100_events.root", "recreate");
+  TTree* tree_ev   = new TTree("evtree", "Looking at the Data Event By Event");
+  TTree* tree_ana  = new TTree("anatree", "Analysis of the Tagger's Performance");
+
+  // Variables for the data that I will place into my tree.
+  std::vector <double> chi_squared_value;
+  std::vector <double> x_axis_offset, y_axis_offset, z_axis_offset;
+  // double x_vertex_value_sce_and_offset_corrected;
+  double x_vertex_value;
+  double x_vertex_point_check;
+  double x_point_value;
+  double y_point_value;
+  double z_point_value; // These both have to be declared here so that they can be used in the truth declaration further down.
+  double min_chi2_value;
+  int    truth_vtx;
+  int    event_number;
+  int    track_number;
+  
+  // Declare values for the branches of the 'tree_ev'.
+  tree_ev->Branch("event_number", &event_number, "event_number/I");
+  tree_ev->Branch("track_number", &track_number, "track_number/I");
+  tree_ev->Branch("chi_squared_value", &chi_squared_value);
+  tree_ev->Branch("x_axis_offset", &x_axis_offset);
+  tree_ev->Branch("y_axis_offset", &y_axis_offset);
+  tree_ev->Branch("z_axis_offset", &z_axis_offset);
+
+  // Declare values for the branches of the 'tree_ana'.
+  // tree_ana->Branch("x_vertex_value_sce_and_offset_corrected", &x_vertex_value_sce_and_offset_corrected, "x_vertex_value_sce_and_offset_corrected/F");
+  tree_ana->Branch("x_vertex_value", &x_vertex_value, "x_vertex_value/D");
+  tree_ana->Branch("x_vertex_point_check", &x_vertex_point_check, "x_vertex_point_check/D"); // This is a check comparing the vertex to the first location in the track.
+  tree_ana->Branch("min_chi2_value", &min_chi2_value, "min_chi2_value/D");
+  tree_ana->Branch("truth_vtx", &truth_vtx, "truth_vtx/I");
 
   // flash metrics output
 
@@ -158,6 +209,12 @@ int main( int nargs, char** argv ) {
   // Import the larlite
 
   for (int ientry=0; ientry<nentries; ientry++) {
+
+    // Set the value of 'event_number'.
+    event_number = ientry;
+
+    std::cout << "Entering into the event loop for event #" << ientry << "." << std::endl;
+    std::cout << "\n" << std::endl;
 
     dataco[kCROIfile].goto_entry(ientry,"larcv");
     dataco[kCROIfile].get_id(run,subrun,event);
@@ -258,6 +315,16 @@ int main( int nargs, char** argv ) {
     // loop through tracks and flash hypotheses. Get metrics for each.
     int num_true_bbox = 0;
     for ( size_t itrack=0; itrack<track_list.size(); itrack++ ) {
+
+      // Set the value of 'track_number'.
+      track_number = itrack;
+
+      // Clear out the vectors that contain the offset information.
+      chi_squared_value.clear();
+      x_axis_offset.clear();
+      y_axis_offset.clear();
+      z_axis_offset.clear();
+      
       const larlite::track& track = *(track_list.at(itrack));
       const larlite::opflash& ophypo = ev_data_ophypo->at(itrack);
       float totpe_data = 0;
@@ -271,12 +338,17 @@ int main( int nargs, char** argv ) {
       // Implement the code for changing the chi2 test at this point in the code.
 
       // First, declare a vector for the offset points at the new point in the square around the central point on the track.
-      std::vector < TVector3 > offset_points_vector;
+      // std::vector < TVector3 > offset_points_vector;
       // std::vector < double > y_offset_vector;
       // std::vector < double > z_offset_vector;
 
       // Declare a vector for the chi2 values at the different points on the track's trajectory.
       std::vector < float > chi_squared_v;
+
+      // Declare a vector for the number of iterations over the points on the grid.
+      int num_iterations = 0;
+
+      larlite::track shifted_track(track);
       
       // The points that are being referenced are with respect to the initial location of these points on the track.
       // Check to see if the change in chi square is constant at all points on the grid.
@@ -287,9 +359,10 @@ int main( int nargs, char** argv ) {
 	  for (double z_offset = -LowestChiSquareGridHalfSize; z_offset <= LowestChiSquareGridHalfSize; z_offset += LowestChiSquareGridPointSep) {
 
 	    // Initialize a new track object with a set of trajectory points as given by the 'offset_vector_points'.                                                               
-	    larlite::track shifted_track(track);
+	    // larlite::track shifted_track(track);
 	    shifted_track.clear_data();
-	    size_t num_of_trajectory_points = track.NumberTrajectoryPoints();
+	    // size_t num_of_trajectory_points = track.NumberTrajectoryPoints();
+	    // shifted_track.reserve(num_of_trajectory_points);
 
 	    // Declare an object of type 'TVector3' that will contain the shifted points on the track which will then be appended to 'offset_points_vector'.
 	    TVector3 shifted_points_set;
@@ -319,19 +392,16 @@ int main( int nargs, char** argv ) {
 
 	      // Append these points onto the 'shifted_track'.
 	      shifted_track.add_vertex(shifted_points_set);
+	      shifted_track.add_direction(track.DirectionAtPoint(point_iterator));	      
 
 	      // Append this object onto the end of the 'offset_points_vector' object (this will occur in order so it will not be a problem).
-	      offset_points_vector.push_back(shifted_points_set);
+	      // offset_points_vector.push_back(shifted_points_set);
 
 	    }
 
 	    // If 'isShiftedTrackOutOfRange' is now 'true', then you can continue onto the next set of shifted points that we will consider.
 	    if (isShiftedTrackOutOfRange)
 	      continue;
-
-	    // Reset the values of the coordinates for the 'shifted_track'.
-	    // shifted_track.fXYZ = shifted_points_set;
-	    // .fXYZ(shifted_points_set);
 
 	    // Turn this track into an object of type 'flashana::QCluster_t' by using the function 'TaggerFlashMatchAlgo::GenerateQCluster'.
 	    flashana::QCluster_t shifted_track_qcluster = flash_matcher_algo.GenerateQCluster(shifted_track);
@@ -347,6 +417,17 @@ int main( int nargs, char** argv ) {
 	    float totpe_hypo_shifted = 0.;
 
 	    float smallest_chi2_shifted = larlitecv::CalculateFlashMatchChi2( intime_data, shifted_track_ophypo, totpe_data_shifted, totpe_hypo_shifted, 20000.0, false );
+	    num_iterations++;
+
+	    // Set the value for the chi-squared calculation.
+	    chi_squared_value.push_back(smallest_chi2_shifted);
+	    x_axis_offset.push_back(x_offset);
+	    y_axis_offset.push_back(y_offset);
+	    z_axis_offset.push_back(z_offset);
+	    
+	    // std::cout << "The new chi squared value = " << smallest_chi2_shifted << "." << std::endl;
+	    // std::cout << "The number of iterations this is = " << num_iterations << "." << std::endl;
+	    // std::cout << "\n" << std::endl;
 
 	    // Append this value onto 'chi_squared_v'.
 	    chi_squared_v.push_back(smallest_chi2_shifted);
@@ -357,6 +438,39 @@ int main( int nargs, char** argv ) {
 
       }
 
+      // Print out 'chi_squared_v'.
+      // std::cout << "The length of the vector of chi squared values is: " << chi_squared_v.size() << "." << std::endl;
+      // std::cout << "\n" << std::endl;     
+
+      // I will run a loop to make sure the output that we have is minimized. I declared this variable further up in my code as well to avoid declaration errors.
+      min_chi2_value  = 10000.;
+      // TVector3 min_chi2_coords = shifted_track.LocationAtPoint(0);
+
+      // Loop through the chi2 values to see what the minimum value is.
+      for (int chi2_iter = 0; chi2_iter < chi_squared_v.size(); chi2_iter++) {
+
+	// std::cout << "chi2 iter: " << chi2_iter << "." << std::endl;
+	// std::cout << "chi2 value: " << chi_squared_v.at(chi2_iter) << "." << std::endl;
+	// std::cout << "\n" << std::endl;
+
+	// Check to see if the value of 'chi_squared_v' at this index is less than 'min_chi2_value'.  Set it equal to 'min_chi2_value' if it is.
+	if (chi_squared_v.at(chi2_iter) < min_chi2_value) {
+
+	  // std::cout << "The new minimum chi2 value = " << chi_squared_v.at(chi2_iter) << "." << std::endl;
+
+	  min_chi2_value  = chi_squared_v.at(chi2_iter);
+	  // min_chi2_coords = shifted_track.LocationAtPoint(chi2_iter);
+
+	}
+
+      }
+
+      // std::cout << "The minimum chi2 value in the vicinity of this track is = " << min_chi2_value << "." << std::endl;
+      // std::cout << "\n" << std::endl;
+
+      // std::cout << "The chi2 value for this track at its original location = " << smallest_chi2 << "." << std::endl;
+      // std::cout << "\n" << std::endl;
+
 	    // Make a copy of the current value of the track using the copy track constructor.
             // track(const track& original) :   
             //  data_base(original),
@@ -366,7 +480,7 @@ int main( int nargs, char** argv ) {
 	    //  fdQdx(original.fdQdx),
 	    //  fFitMomentum(original.fFitMomentum),
 	    //  fID(original.fID)
-
+      
       // need to determine if this track is the intime track or not
       // we do this by checking if track gets close to true vertex. slow AF.
       std::vector< std::vector<float> > bbox = larlitecv::TaggerFlashMatchAlgo::GetAABoundingBox( track );
@@ -376,6 +490,37 @@ int main( int nargs, char** argv ) {
       apparent_vtx[0] = truthdata.pos[0] - sce_offsets[0] - 0.17;
       apparent_vtx[1] = truthdata.pos[1] + sce_offsets[1];
       apparent_vtx[2] = truthdata.pos[2] + sce_offsets[2];
+
+      // I can set the values for the truth vertex and the 'corrected' vertex here.
+      // x_vertex_value                          = truthdata.pos[0];
+      // x_vertex_value_sce_and_offset_corrected = apparent_vtx[0];
+
+      x_vertex_value       = track.Vertex()[0];
+      x_vertex_point_check = track.LocationAtPoint(0)[0];
+      // y_vertex_value = track.Vertex()[1];
+      // z_vertex_value = track.Vertex()[2];
+      
+
+      // Print these two values.
+      // std::cout << "x vertex value = " << x_vertex_value << "." << std::endl;
+      // std::cout << "x corrected vertex value = " << x_vertex_value_sce_and_offset_corrected << "." << std::endl;
+
+      // Set 'truth_vtx' to 0.
+      truth_vtx = 0;
+
+      for (size_t track_point_iter = 0; track_point_iter < track.NumberTrajectoryPoints(); track_point_iter++) {
+
+	// Declare the points that will be compared to the 'apparent_vtx' values.
+	x_point_value = track.LocationAtPoint(track_point_iter)[0];
+	y_point_value = track.LocationAtPoint(track_point_iter)[1];
+	z_point_value = track.LocationAtPoint(track_point_iter)[2];
+
+	// Check if this is a true vertex by seeing if it is 5 cm from the 'apparent_vtx' values.
+	if (fabs(apparent_vtx[0] - x_point_value) < 5.0 && fabs(apparent_vtx[1] - y_point_value) < 5.0 && fabs(apparent_vtx[2] - z_point_value) < 5.0)
+	  truth_vtx = 1;
+
+      }
+      
       for ( int v=0; v<3; v++ ) {
         if ( apparent_vtx[v]<bbox[v][0]-bbox_buffer || apparent_vtx[v]>bbox[v][1]+bbox_buffer ) {
           inbbox = false;
@@ -388,6 +533,10 @@ int main( int nargs, char** argv ) {
 	contained = true;
 
       if ( inbbox ) {
+
+	// If this loop is entered, then it is a real vertex. Set 'truth_vtx' to 1.
+	// truth_vtx = 1;
+	
         smallest_chi2_trueflash_v.push_back( smallest_chi2 );
         smallest_gausll_trueflash_v.push_back( smallest_gausll );
         totpe_hypo_trueflash_v.push_back( totpe_hypo );
@@ -407,16 +556,27 @@ int main( int nargs, char** argv ) {
 	  containment_falseflash_v.push_back( 0 );	
       }
 
+      // Print the x-coordinate value.
+      std::cout << "x-coordinate value = " << x_vertex_value << "." << std::endl;
+      std::cout << "minimum chi2 value = " << min_chi2_value << "." << std::endl;
+      std::cout << "\n" << std::endl;
+
+      // Fill the 'tree_ana' and 'tree_ev' here for each track in the set of events.
+      tree_ana->Fill();
+      tree_ev->Fill();
+      
     }
     std::cout << "Number of Vertex Enclosing BBox: " << num_true_bbox << std::endl;
 
     tree->Fill();
 
-    //if ( ientry>=3)
-    //  break;
+    // I will only use 10 entries for now.
+    if ( ientry>=99)
+      break;
   }//end of entry loop
 
   rfile->Write();
+  rfile_ana->Write();
   return 0;
 
 }//end of main
