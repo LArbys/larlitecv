a427f7e393b4b7239df767d41540547abd7f5eb5
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-05-31 16:54:15 -0400

    formatting

--- app/ContainedROI/TaggerFlashMatchAlgoConfig.h
+++ app/ContainedROI/TaggerFlashMatchAlgoConfig.h
@@ -11,33 +11,33 @@
 
 namespace larlitecv {
 
-	class TaggerFlashMatchAlgoConfig {
+  class TaggerFlashMatchAlgoConfig {
   public:
-  	TaggerFlashMatchAlgoConfig();
-  	virtual ~TaggerFlashMatchAlgoConfig() {};
-
-  	int verbosity;
-  	float qcluster_stepsize;
-  	float MeV_per_cm;
-  	float fudge_factor;
-	float fudge_factor_cosmic;
-  	float us_per_tick;
-  	float pmtflash_thresh;
-  	float flashmatch_chi2_cut;
-  	float flashpe_thresh;
-	float totpe_sigma_cut;
-	float bbox_pad;
-	bool use_gaus2d;
-  	std::vector<int> beam_tick_range;
-  	std::vector<float> FVCutX;
-  	std::vector<float> FVCutY;
-  	std::vector<float> FVCutZ;
-  	std::vector<float> gain_correction;
-  	fcllite::PSet m_flashmatch_config;
-  	static const std::string m_flashman_default;
-
-  	static TaggerFlashMatchAlgoConfig MakeTaggerFlashMatchAlgoConfigFromPSet( const larcv::PSet& pset );
-	};
+    TaggerFlashMatchAlgoConfig();
+    virtual ~TaggerFlashMatchAlgoConfig() {};
+    
+    int verbosity;
+    float qcluster_stepsize;
+    float MeV_per_cm;
+    float fudge_factor;
+    float fudge_factor_cosmic;
+    float us_per_tick;
+    float pmtflash_thresh;
+    float flashmatch_chi2_cut;
+    float flashpe_thresh;
+    float totpe_sigma_cut;
+    float bbox_pad;
+    bool use_gaus2d;
+    std::vector<int> beam_tick_range;
+    std::vector<float> FVCutX;
+    std::vector<float> FVCutY;
+    std::vector<float> FVCutZ;
+    std::vector<float> gain_correction;
+    fcllite::PSet m_flashmatch_config;
+    static const std::string m_flashman_default;
+    
+    static TaggerFlashMatchAlgoConfig MakeTaggerFlashMatchAlgoConfigFromPSet( const larcv::PSet& pset );
+  };
 
 }
 
