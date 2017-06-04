b2f7ba951a76e24ac9b564cd5a53661170ae5c46
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-06-04 17:36:07 -0400

    dont pop last step. formatting

--- app/StopMu/StopMuFoxTrot.cxx
+++ app/StopMu/StopMuFoxTrot.cxx
@@ -22,12 +22,12 @@ namespace larlitecv {
     // incorporating tagged information
     // we mask out the charge information, but we also put that information
     // into a badchannel image as well (later if needed)
-    
+
     for ( auto const& startpt : startpts_v ) {
 
       FoxTrack ft = m_algo->followTrack( img_v, badch_v, thrumu_v, startpt );
       if ( (int)ft.size()<m_config.min_num_steps )
-	continue;
+        continue;
 
       // if long enough, we continue the end of the track to find the end (not done)
 
@@ -35,31 +35,31 @@ namespace larlitecv {
       std::vector<int> imgcoords = larcv::UBWireTool::getProjectedImagePixel( ft.back().pos(), img_v.front().meta(), img_v.size() );
       std::vector<BoundaryEndPt> bendpt_v;
       for ( int p=0; p<(int)img_v.size(); p++ ) {
-	//const larcv::ImageMeta& meta = img_v[p].meta();
-	BoundaryEndPt bendpt( imgcoords[0], imgcoords[p+1], larlitecv::kUndefined ); // for stopmu end
-	bendpt_v.emplace_back( std::move(bendpt) );
+        //const larcv::ImageMeta& meta = img_v[p].meta();
+        BoundaryEndPt bendpt( imgcoords[0], imgcoords[p+1], larlitecv::kUndefined ); // for stopmu end
+        bendpt_v.emplace_back( std::move(bendpt) );
       }
       BoundarySpacePoint endpt( larlitecv::kUndefined, std::move(bendpt_v), ft.back().pos()[0], ft.back().pos()[1], ft.back().pos()[2] );
 
       // pop the end
-      ft.pop_back();
+      //ft.pop_back();
 
       std::vector< std::vector<double> > path3d;
       path3d.reserve( ft.size() );
-      
+
       for ( auto& foxstep : ft ) {
-	if ( !foxstep.isgood() )
-	  continue;
-	std::vector<double> posd(foxstep.pos().size(),0);
-	//std::cout << "posd: (";
-	for (int i=0; i<(int)foxstep.pos().size(); i++) {
-	  posd[i] = foxstep.pos()[i];
-	  //std::cout << posd[i] << " ";
-	}
-	//std::cout << ")" << std::endl;
-	path3d.push_back( posd );
+        if ( !foxstep.isgood() )
+          continue;
+        std::vector<double> posd(foxstep.pos().size(),0);
+        //std::cout << "posd: (";
+        for (int i=0; i<(int)foxstep.pos().size(); i++) {
+          posd[i] = foxstep.pos()[i];
+          //std::cout << posd[i] << " ";
+        }
+        //std::cout << ")" << std::endl;
+        path3d.push_back( posd );
       }
-      
+
       BMTrackCluster3D track3d( startpt, endpt, path3d );
       //std::cout << "Stopmu foxtrot track has " << path3d.size() << " steps" << std::endl;
       track3d_v.emplace_back( std::move(track3d) );
@@ -67,5 +67,5 @@ namespace larlitecv {
 
     return track3d_v;
   }
-  
+
 }
