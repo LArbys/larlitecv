1db84a944f6a75efc892af0c4020ca426beccaae
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-06-04 17:34:41 -0400

    added dwall function for specific boundary

--- app/TaggerTypes/dwall.cxx
+++ app/TaggerTypes/dwall.cxx
@@ -1,6 +1,7 @@
 #include "dwall.h"
 
 #include <cmath>
+#include <stdexcept>
 
 namespace larlitecv {
 
@@ -48,6 +49,51 @@ namespace larlitecv {
     }
 
     return dwall;
+    
+  }
+
+  double dspecificwall( const std::vector<double>& pos, const int boundary_type ) {
+    std::vector<float> fpos(3);
+    for (int i=0; i<3; i++)
+      fpos[i] = (float)pos[i];
+    return dspecificwall( fpos, boundary_type);
+  }
+  
+  float dspecificwall( const std::vector<float>& pos, const int boundary_type ) {
+
+    float dx1 = fabs(pos[0]);
+    float dx2 = fabs(258-pos[0]);
+    float dy1 = fabs(117.0-pos[1]);
+    float dy2 = fabs(-117.0-pos[1]);
+    float dz1 = fabs(pos[2]);
+    float dz2 = fabs(1036.0-pos[2]);
+
+    float fdwall = 1.0e9;
+
+    switch ( boundary_type ) {
+    case 0:
+      fdwall = dy1;
+      break;
+    case 1:
+      fdwall = dy2;
+      break;
+    case 2:
+      fdwall = dz1;
+      break;
+    case 3:
+      fdwall = dz2;
+      break;
+    case 4:
+      fdwall = dx1;
+      break;
+    case 5:
+      fdwall = dx2;
+      break;
+    default:
+      throw std::runtime_error("dspecifcwall: imageend boundary points undefined");
+    }
+
+    return fdwall;
     
   }
   
--- app/TaggerTypes/dwall.h
+++ app/TaggerTypes/dwall.h
@@ -6,7 +6,10 @@
 namespace larlitecv {
   
   float dwall( const std::vector<float>& pos, int& boundary_type );
-  double dwall( const std::vector<double>& pos, int& boundary_type );  
+  double dwall( const std::vector<double>& pos, int& boundary_type );
+
+  float dspecificwall( const std::vector<float>& pos, const int boundary_type );
+  double dspecificwall( const std::vector<double>& pos, const int boundary_type );
   
 }
 
