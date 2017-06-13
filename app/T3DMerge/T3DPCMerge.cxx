14db39061593fd77a2e0cbbfc94d8663f11d8073
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-06-13 02:20:01 -0400

    rearrange verbose output

--- app/T3DMerge/T3DPCMerge.cxx
+++ app/T3DMerge/T3DPCMerge.cxx
@@ -8,7 +8,7 @@ namespace larlitecv {
     m_max_iterations = 100;
     m_verbose = 0;
   }
-  
+
   std::vector<T3DCluster> T3DPCMerge::merge( const std::vector<T3DCluster>& tracks ) {
 
     // techniques we use:
@@ -19,7 +19,7 @@ namespace larlitecv {
     //std::vector<T3DCluster> submerged = mergeSubTracks( endpoint_merged );
 
     return endpoint_merged;
-    
+
   }
 
   std::vector< T3DCluster > T3DPCMerge::endPointMerge( const std::vector<T3DCluster>& tracks ) {
@@ -35,92 +35,133 @@ namespace larlitecv {
     std::vector<T3DCluster*> current_list;
     for (auto& track : tracklist )
       current_list.push_back( &track );
-    
+
     do {
       performed_merge = false;
 
       std::vector<T3DCluster*> merge_list;
 
       for (int i=0; i<(int)current_list.size(); i++) {
-	for (int j=i+1; j<(int)current_list.size(); j++) {
+      	for (int j=i+1; j<(int)current_list.size(); j++) {
 
-	  T3DCluster& track_i = *(current_list[i]);
-	  T3DCluster& track_j = *(current_list[j]);
+      	  T3DCluster& track_i = *(current_list[i]);
+      	  T3DCluster& track_j = *(current_list[j]);
+
+          // --------------------------------------------------------------------
+          // Check quality of tracks
+
+          // size: tracks need to be made up of more than one point
+          if ( track_i.getPath().size()<2 )
+            continue;
+          if ( track_j.getPath().size()<2 )
+            continue;
 
 
-	  // --------------------------------------------------------------------
-	  // END POINT MERGE
-	  double closest_dist = 1.0e6;
-	  std::vector<int> whichends;	  
-	  bool valid = shouldWeEndPointMerge( track_i, track_j, closest_dist, whichends );
+      	  // --------------------------------------------------------------------
+      	  // END POINT MERGE
+      	  double closest_dist = 1.0e6;
+      	  std::vector<int> whichends;
+	  if ( m_verbose>1 )
+	    std::cout << "Testing (" << i << "," << j << ")" << std::endl;
+      	  bool valid = shouldWeEndPointMerge( track_i, track_j, closest_dist, whichends );
 
-	  if ( valid  ) {
-	    performed_merge = true;
-	    if ( whichends[0]==0 && whichends[1]==0 ) {
-	      // start/start
-	      track_i.reverse();
-	      track_i.append( track_j );
-	      track_i.reverse();
-	    }
-	    else if ( whichends[0]==1 && whichends[1]==0 ) {
-	      // end/start
-	      track_i.append( track_j );
-	    }
-	    else if ( whichends[0]==0 && whichends[1]==1 ) {
-	      // start/end
-	      track_j.append( track_i );
-	      std::swap( track_i, track_j ); // dangerous?
-	    }
-	    else if ( whichends[0]==1 && whichends[1]==1 ) {
-	      //end/end
-	      track_j.reverse();
-	      track_i.append( track_j );
-	    }
-	  }
+      	  if ( valid  ) {
+      	    performed_merge = true;
+      	    if ( whichends[0]==0 && whichends[1]==0 ) {
+      	      // start/start
+      	      track_i.reverse();
+      	      track_i.append( track_j );
+      	      track_i.reverse();
+      	    }
+      	    else if ( whichends[0]==1 && whichends[1]==0 ) {
+      	      // end/start
+      	      track_i.append( track_j );
+      	    }
+      	    else if ( whichends[0]==0 && whichends[1]==1 ) {
+      	      // start/end
+      	      track_j.append( track_i );
+      	      std::swap( track_i, track_j ); // dangerous?
+      	    }
+      	    else if ( whichends[0]==1 && whichends[1]==1 ) {
+      	      //end/end
+      	      track_j.reverse();
+      	      track_i.append( track_j );
+      	    }
+      	  }
 
-	  // --------------------------------------------------------------------	  
-	  // GATHER MERGED TRACKS
-	  if ( performed_merge ) {
-	    // fill the merge list to go to the next round
-	    merge_list.push_back( &track_i );	    
-	    for ( auto& ptrack : current_list ) {
-	      if ( ptrack==&track_i || ptrack==&track_j )
-		continue;
-	      merge_list.push_back( ptrack );
-	    }
-	    break;
-	  }
-	}
-	
-	if ( performed_merge )
-	  break;	
+      	  // --------------------------------------------------------------------
+      	  // GATHER MERGED TRACKS
+      	  if ( performed_merge ) {
+      	    // fill the merge list to go to the next round
+      	    merge_list.push_back( &track_i );
+      	    for ( auto& ptrack : current_list ) {
+      	      if ( ptrack==&track_i || ptrack==&track_j )
+      		continue;
+      	      merge_list.push_back( ptrack );
+      	    }
+      	    break;
+      	  }
+      	}
+
+      	if ( performed_merge )
+      	  break;
       }
-      
+
       // update track list
       if ( performed_merge ) {
-	current_list.clear();
-	current_list = merge_list;
+      	current_list.clear();
+      	current_list = merge_list;
       }
-      
+
       iter++;
     } while ( performed_merge && iter<m_max_iterations );
-    
+
 
     // copy current track list objects to output vector
     std::vector<T3DCluster> out;
     for ( auto& ptrack : current_list ) {
       out.push_back( *ptrack );
     }
-    return out;    
+    return out;
   }
-  
-  bool T3DPCMerge::shouldWeEndPointMerge( const T3DCluster& ta, const T3DCluster& tb, double& closest_dist, std::vector<int>& whichends ) {
 
+  bool T3DPCMerge::shouldWeEndPointMerge( const T3DCluster& ta, const T3DCluster& tb, double& closest_dist, std::vector<int>& whichends ) {
+    
     if ( m_verbose>1 ) {
       std::cout << "----------------------" << std::endl;
       std::cout << " should we merge" << std::endl;
-    }
+      std::cout << "  TA size: " << ta.getPath().size() << std::endl;
+      std::cout << "  TB size: " << tb.getPath().size() << std::endl;
+      std::cout << "  TA: (" << ta.getPath().front()[0] << "," << ta.getPath().front()[1] << "," << ta.getPath().front()[2] << ") -> "
+    		<< "(" << ta.getPath().back()[0] << "," << ta.getPath().back()[1] << "," << ta.getPath().back()[2] << ")"
+    		<< std::endl;
+      std::cout << "  TB: (" << tb.getPath().front()[0] << "," << tb.getPath().front()[1] << "," << tb.getPath().front()[2] << ") -> "
+    		<< "(" << tb.getPath().back()[0] << "," << tb.getPath().back()[1] << "," << tb.getPath().back()[2] << ")"
+    		<< std::endl;
 
+      // std::cout << "  TA PCA0: (" << linea.Pt1()[0] << "," << linea.Pt1()[1] << "," << linea.Pt1()[2] << ") -> "
+      // 		<< "(" << linea.Pt2()[0] << "," << linea.Pt2()[1] << "," << linea.Pt2()[2] << ")"
+      // 		<< std::endl;
+      // std::cout << "  TB PCA0: (" << lineb.Pt1()[0] << "," << lineb.Pt1()[1] << "," << lineb.Pt1()[2] << ") -> "
+      // 		<< "(" << lineb.Pt2()[0] << "," << lineb.Pt2()[1] << "," << lineb.Pt2()[2] << ")"
+      // 		<< std::endl;
+
+      std::cout << "  TA pc-0 dir: (" << ta.getPCADir(0)[0] << "," << ta.getPCADir(0)[1] << "," << ta.getPCADir(0)[2] << ")" << std::endl;
+      std::cout << "  TB pc-0 dir: (" << tb.getPCADir(0)[0] << "," << tb.getPCADir(0)[1] << "," << tb.getPCADir(0)[2] << ")" << std::endl;
+      std::cout << "  TA mean: (" << ta.getMean()[0] << "," << ta.getMean()[1] << "," << ta.getMean()[2] << ")" << std::endl;
+      std::cout << "  TB mean: (" << tb.getMean()[0] << "," << tb.getMean()[1] << "," << tb.getMean()[2] << ")" << std::endl;
+
+      // std::cout << "  Mid point between close end: (" << midpt[0] << "," << midpt[1] << "," << midpt[2] << ") " << std::endl;
+      // std::cout << "  closest point distance=" << closest_dist << " cm" << std::endl;
+      std::cout << "--[TB]-------------" << std::endl;
+      for (int i=0; i<(int)tb.getPath().size(); i++) {
+	const auto& tbpt = tb.getPath()[i];
+	std::cout << "  #" << i << ": (" << tbpt[0] << "," << tbpt[1] << "," << tbpt[2] << ")" << std::endl;
+      }
+      
+    }
+    //return false;// bail!
+    
     // Make PCA line
     std::vector<double> staa(3);
     std::vector<double> stab(3);
@@ -129,7 +170,7 @@ namespace larlitecv {
     for (int i=0; i<3; i++) {
       staa[i] = ta.getMean()[i]+ta.getPCABounds(0)[0]*ta.getPCADir(0)[i];
       enda[i] = ta.getMean()[i]+ta.getPCABounds(0)[1]*ta.getPCADir(0)[i];
-      
+
       stab[i] = tb.getMean()[i]+tb.getPCABounds(0)[0]*tb.getPCADir(0)[i];
       endb[i] = tb.getMean()[i]+tb.getPCABounds(0)[1]*tb.getPCADir(0)[i];
     }
@@ -146,11 +187,11 @@ namespace larlitecv {
     }
     enum EndType_t { kStartStart=0, kStartEnd, kEndStart, kEndEnd, kNumEndTypes };
     EndType_t endtype = kStartStart;
-    double endmin = enddist[0];    
+    double endmin = enddist[0];
     for (int i=kStartEnd;i<kNumEndTypes; i++) {
       if ( enddist[i]<endmin ) {
-	endtype = (EndType_t)i;
-	endmin = enddist[i];
+      	endtype = (EndType_t)i;
+      	endmin = enddist[i];
       }
     }
     endmin = sqrt(endmin);
@@ -167,46 +208,46 @@ namespace larlitecv {
     for (int i=0; i<3; i++) {
       switch (endtype) {
       case kStartStart:
-	midpt[i] = 0.5*(ta.getPath().front()[i]+tb.getPath().front()[i]);
-	whichends[0] = 0;
-	whichends[1] = 0;	
-	break;
+      	midpt[i] = 0.5*(ta.getPath().front()[i]+tb.getPath().front()[i]);
+      	whichends[0] = 0;
+      	whichends[1] = 0;
+      	break;
       case kStartEnd:
-	midpt[i] = 0.5*(ta.getPath().front()[i]+tb.getPath().back()[i]);
-	whichends[0] = 0;
-	whichends[1] = 1;
-	break;
+        midpt[i] = 0.5*(ta.getPath().front()[i]+tb.getPath().back()[i]);
+        whichends[0] = 0;
+        whichends[1] = 1;
+        break;
       case kEndStart:
-	midpt[i] = 0.5*(ta.getPath().back()[i]+tb.getPath().front()[i]);
-	whichends[0] = 1;
-	whichends[1] = 0;	
-	break;
+      	midpt[i] = 0.5*(ta.getPath().back()[i]+tb.getPath().front()[i]);
+      	whichends[0] = 1;
+      	whichends[1] = 0;
+      	break;
       case kEndEnd:
-	midpt[i] = 0.5*(ta.getPath().back()[i]+tb.getPath().back()[i]);
-	whichends[0] = 1;
-	whichends[1] = 1;
-	break;
+      	midpt[i] = 0.5*(ta.getPath().back()[i]+tb.getPath().back()[i]);
+      	whichends[0] = 1;
+      	whichends[1] = 1;
+      	break;
       case kNumEndTypes:
       default:
-	throw std::runtime_error("bad end type");
-	break;
+      	throw std::runtime_error("bad end type");
+      	break;
       }//end of switch
       // cruft: in case we do this using ends of the first-PC
       // case 0:
       // 	midpt[i] = 0.5*(linea.Pt1()[i]+lineb.Pt1()[i]);
-      // 	l2pt[i] = 0.5*(linea.Pt1()[i]+lineb.Pt1()[i]);	
+      // 	l2pt[i] = 0.5*(linea.Pt1()[i]+lineb.Pt1()[i]);
       // 	break;
       // case 1:
       // 	midpt[i] = 0.5*(linea.Pt1()[i]+lineb.Pt2()[i]);
-      // 	l2pt[i] = 0.5*(linea.Pt1()[i]+lineb.Pt2()[i]);	
+      // 	l2pt[i] = 0.5*(linea.Pt1()[i]+lineb.Pt2()[i]);
       // 	break;
       // case 2:
       // 	midpt[i] = 0.5*(linea.Pt2()[i]+lineb.Pt1()[i]);
-      // 	l2pt[i] = 0.5*(linea.Pt2()[i]+lineb.Pt1()[i]);	
+      // 	l2pt[i] = 0.5*(linea.Pt2()[i]+lineb.Pt1()[i]);
       // 	break;
       // case 3:
       // 	midpt[i] = 0.5*(linea.Pt2()[i]+lineb.Pt2()[i]);
-      // 	l2pt[i] = 0.5*(linea.Pt2()[i]+lineb.Pt2()[i]);	
+      // 	l2pt[i] = 0.5*(linea.Pt2()[i]+lineb.Pt2()[i]);
       // 	break;
       // default:
       // 	throw std::runtime_error("bad end type");
@@ -216,24 +257,24 @@ namespace larlitecv {
     closest_dist = 0.;
 
     if ( m_verbose>1 ) {
-      std::cout << "TA: (" << ta.getPath().front()[0] << "," << ta.getPath().front()[1] << "," << ta.getPath().front()[2] << ") -> "
-		<< "(" << ta.getPath().back()[0] << "," << ta.getPath().back()[1] << "," << ta.getPath().back()[2] << ")"
-		<< std::endl;
-      std::cout << "TB: (" << tb.getPath().front()[0] << "," << tb.getPath().front()[1] << "," << tb.getPath().front()[2] << ") -> "
-		<< "(" << tb.getPath().back()[0] << "," << tb.getPath().back()[1] << "," << tb.getPath().back()[2] << ")"
-		<< std::endl;
+      // std::cout << "TA: (" << ta.getPath().front()[0] << "," << ta.getPath().front()[1] << "," << ta.getPath().front()[2] << ") -> "
+      // 		<< "(" << ta.getPath().back()[0] << "," << ta.getPath().back()[1] << "," << ta.getPath().back()[2] << ")"
+      // 		<< std::endl;
+      // std::cout << "TB: (" << tb.getPath().front()[0] << "," << tb.getPath().front()[1] << "," << tb.getPath().front()[2] << ") -> "
+      // 		<< "(" << tb.getPath().back()[0] << "," << tb.getPath().back()[1] << "," << tb.getPath().back()[2] << ")"
+      // 		<< std::endl;
 
       std::cout << "TA PCA0: (" << linea.Pt1()[0] << "," << linea.Pt1()[1] << "," << linea.Pt1()[2] << ") -> "
-		<< "(" << linea.Pt2()[0] << "," << linea.Pt2()[1] << "," << linea.Pt2()[2] << ")"
-		<< std::endl;
+    		<< "(" << linea.Pt2()[0] << "," << linea.Pt2()[1] << "," << linea.Pt2()[2] << ")"
+    		<< std::endl;
       std::cout << "TB PCA0: (" << lineb.Pt1()[0] << "," << lineb.Pt1()[1] << "," << lineb.Pt1()[2] << ") -> "
-		<< "(" << lineb.Pt2()[0] << "," << lineb.Pt2()[1] << "," << lineb.Pt2()[2] << ")"
-		<< std::endl;
+    		<< "(" << lineb.Pt2()[0] << "," << lineb.Pt2()[1] << "," << lineb.Pt2()[2] << ")"
+    		<< std::endl;
 
-      std::cout << "TA pc-0 dir: (" << ta.getPCADir(0)[0] << "," << ta.getPCADir(0)[1] << "," << ta.getPCADir(0)[2] << ")" << std::endl;
-      std::cout << "TB pc-0 dir: (" << tb.getPCADir(0)[0] << "," << tb.getPCADir(0)[1] << "," << tb.getPCADir(0)[2] << ")" << std::endl;
-      std::cout << "TA mean: (" << ta.getMean()[0] << "," << ta.getMean()[1] << "," << ta.getMean()[2] << ")" << std::endl;
-      std::cout << "TB mean: (" << tb.getMean()[0] << "," << tb.getMean()[1] << "," << tb.getMean()[2] << ")" << std::endl;
+      // std::cout << "TA pc-0 dir: (" << ta.getPCADir(0)[0] << "," << ta.getPCADir(0)[1] << "," << ta.getPCADir(0)[2] << ")" << std::endl;
+      // std::cout << "TB pc-0 dir: (" << tb.getPCADir(0)[0] << "," << tb.getPCADir(0)[1] << "," << tb.getPCADir(0)[2] << ")" << std::endl;
+      // std::cout << "TA mean: (" << ta.getMean()[0] << "," << ta.getMean()[1] << "," << ta.getMean()[2] << ")" << std::endl;
+      // std::cout << "TB mean: (" << tb.getMean()[0] << "," << tb.getMean()[1] << "," << tb.getMean()[2] << ")" << std::endl;
 
       std::cout << "Mid point between close end: (" << midpt[0] << "," << midpt[1] << "," << midpt[2] << ") " << std::endl;
       std::cout << "closest point distance=" << closest_dist << " cm" << std::endl;
@@ -243,6 +284,7 @@ namespace larlitecv {
     float pcacos = 0.;
     for (int i=0; i<3; i++)
       pcacos += ta.getPCADir(0)[i]*tb.getPCADir(0)[i];
+
     // correct sign based on track orientations
     switch (endtype) {
     case kStartStart:
@@ -254,10 +296,10 @@ namespace larlitecv {
     case kNumEndTypes:
       break;
     }
-    
+
     if ( m_verbose>1 )
       std::cout << "PCA cosine: " << pcacos << std::endl;
-    
+
     // midpt and l2pt need to extend beyond
     double pca_l1 = ta.getPCvalue(midpt,0);
     double pca_l2 = tb.getPCvalue(midpt,0);
@@ -275,14 +317,14 @@ namespace larlitecv {
       std::cout << "TA: dist to midpoint=" << middist_a << " cm" << std::endl;
       std::cout << "TB: dist to midpoint=" << middist_b << " cm" << std::endl;
     }
-    
+
     if ( (pca_l1<ta.getPCABounds(0)[0] || pca_l1>ta.getPCABounds(0)[1] )
-	 && (pca_l2<tb.getPCABounds(0)[0] || pca_l2>tb.getPCABounds(0)[1] ) ) {
+      && (pca_l2<tb.getPCABounds(0)[0] || pca_l2>tb.getPCABounds(0)[1] ) ) {
       outside_segment = true;
     }
     if ( m_verbose>1 )
       std::cout << "Is midpoint outside segment: " << outside_segment << std::endl;
-    
+
 
     if ( m_verbose>1 )
       std::cout << "End Matching Code: A=" << whichends[0] << " B=" << whichends[1] << std::endl;
@@ -320,5 +362,5 @@ namespace larlitecv {
       track_i.append( track_j );
     }
   }
-  
+
 }
