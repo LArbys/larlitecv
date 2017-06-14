dac8697d3362cec02b17f4919d57a1b23f99a171
Christopher Barnes <barnchri@barnchri-mbp.guest-network.net>
2017-06-14 12:06:17 -0500

    Checking for Out-of-Bounds Pixels in the Trackers

--- app/ThruMu/AStarNodes2BMTrackCluster3D.cxx
+++ app/ThruMu/AStarNodes2BMTrackCluster3D.cxx
@@ -4,6 +4,9 @@
 #include "LArUtil/LArProperties.h"
 #include "LArUtil/Geometry.h"
 
+// larcv (crb)
+#include "UBWireTool/UBWireTool.h"
+
 namespace larlitecv {
 
   BMTrackCluster3D AStarNodes2BMTrackCluster3D( const std::vector<AStar3DNode>& path, const std::vector<larcv::Image2D>& img_v,
@@ -48,14 +51,42 @@ namespace larlitecv {
       for (int istep=0; istep<=nsteps; istep++) {
         Double_t xyz[3];
         std::vector<double> pt(3,0.0);
+
+	// Give the corresponding values as floats to eliminate the error.
+	Float_t xyz_float[3];
+	std::vector<float> pt_float(3,0.0);
+	
         for (int i=0; i<3; i++) {
           xyz[i] = step0[i] + stepsize*istep*dir3d[i];
           pt[i] = xyz[i];
+
+	  xyz_float[i] = step0[i] + stepsize*istep*dir3d[i];
+          pt_float[i] = xyz_float[i];
         }
 
-        output_path.emplace_back( std::move(pt) );
+	// Ensure that the 3 coordinates give a value inside the image before appending this value to the track.
+	// Convert the 3D position to wire coordinates (I have to use xyz to do this).
+	std::vector<int> imgcoords = larcv::UBWireTool::getProjectedImagePixel( pt_float, img_v.front().meta(), img_v.size() );
 
-      }//end of steps
+	// Continue if the imgcoords are not inside the image.
+	// If any dimension is outside the image, then please continue.
+
+	bool point_gives_2D_pixel_out_of_range = false;
+	
+	if (imgcoords[0] < 0 || imgcoords[0] >= img_v.at(0).meta().rows())
+	  point_gives_2D_pixel_out_of_range = true;
+
+	for (size_t p = 0; p < img_v.size(); p++) {
+
+	  if (imgcoords[p+1] < 0 || imgcoords[p+1] >= img_v.at(0).meta().cols())
+	    point_gives_2D_pixel_out_of_range = true;
+
+	}
+
+	if (!point_gives_2D_pixel_out_of_range)
+	  output_path.emplace_back( std::move(pt) );
+
+      } //end of steps
 
     }//end of node loop
 
