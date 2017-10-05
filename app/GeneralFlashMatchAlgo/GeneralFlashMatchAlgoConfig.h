bd91054d7a100dfcdabf35a054b9ec8f6d04f6bc
barnchri <barnchri@umich.edu>
2017-10-05 17:11:49 -0400

    Placing all of the flash-matching for the CROIs into GeneralFlashMatchAlgo.

--- app/GeneralFlashMatchAlgo/GeneralFlashMatchAlgoConfig.h
+++ app/GeneralFlashMatchAlgo/GeneralFlashMatchAlgoConfig.h
@@ -18,12 +18,22 @@ namespace larlitecv {
 
     float chi2_anode_cathode_cut;
     float chi2_yz_flash_cut;
-    int verbosity;
+    int   verbosity;
     float qcluster_stepsize;
     float MeV_per_cm;
     float fudge_factor;
+    float fudge_factor_cosmic;
+    float us_per_tick;
     float pmtflash_thresh;
-    bool use_gaus2d;
+    float flashmatch_chi2_cut;
+    float flashpe_thresh;
+    float totpe_sigma_cut;
+    float bbox_pad;
+    bool  use_gaus2d;
+    std::vector<int> beam_tick_range;
+    std::vector<float> FVCutX;
+    std::vector<float> FVCutY;
+    std::vector<float> FVCutZ;
     fcllite::PSet m_flashmatch_config;
     std::vector < float > gain_correction;
     static const std::string m_flashman_default;
