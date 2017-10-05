efedb73e31f144c4f297793b498c5b0957821e08
Victor Genty <vgenty@nevis.columbia.edu>
2017-10-05 11:20:31 -0400

    shower reco processor compatibility

--- core/Hashlib2plus/LinkDef.h
+++ core/Hashlib2plus/LinkDef.h
@@ -1,18 +1,23 @@
-//
-// cint script to generate libraries
-// Declaire namespace & classes you defined
-// #pragma statement: order matters! Google it ;)
-//
-
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-// Classes
-//ADD_NEW_CLASS ... do not change this line
+#pragma link C++ class hlException+;
+#pragma link C++ class hashwrapper+;
+#pragma link C++ class MD5+;
+#pragma link C++ class md5wrapper+;
+#pragma link C++ class SHA1+;
+#pragma link C++ class sha1wrapper+;
+#pragma link C++ class SHA256+;
+#pragma link C++ class sha256wrapper+;
+#pragma link C++ class SHA2ext+;
+#pragma link C++ class sha384wrapper+;
+#pragma link C++ class sha512wrapper+;
+#pragma link C++ class wrapperfactory+;
 
 #endif
+
 
 
 
