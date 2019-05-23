a653e37dada2f43d41e2b592b096ebdb05d8bd54
twongjirad <taritree.wongjirad@gmail.com>
2019-05-23 12:15:19 -0400

    removed hashlib classes from dict. we dont need them there. causing compilation problems due to overlapping macro names with ROOT

--- core/Hashlib2plus/LinkDef.h
+++ core/Hashlib2plus/LinkDef.h
@@ -3,18 +3,18 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class hlException+;
-#pragma link C++ class hashwrapper+;
-#pragma link C++ class MD5+;
-#pragma link C++ class md5wrapper+;
-#pragma link C++ class SHA1+;
-#pragma link C++ class sha1wrapper+;
-#pragma link C++ class SHA256+;
-#pragma link C++ class sha256wrapper+;
-#pragma link C++ class SHA2ext+;
-#pragma link C++ class sha384wrapper+;
-#pragma link C++ class sha512wrapper+;
-#pragma link C++ class wrapperfactory+;
+# #pragma link C++ class hlException+;
+# #pragma link C++ class hashwrapper+;
+# #pragma link C++ class MD5+;
+# #pragma link C++ class md5wrapper+;
+# #pragma link C++ class SHA1+;
+# #pragma link C++ class sha1wrapper+;
+# #pragma link C++ class SHA256+;
+# #pragma link C++ class sha256wrapper+;
+# #pragma link C++ class SHA2ext+;
+# #pragma link C++ class sha384wrapper+;
+# #pragma link C++ class sha512wrapper+;
+# #pragma link C++ class wrapperfactory+;
 
 #endif
 
